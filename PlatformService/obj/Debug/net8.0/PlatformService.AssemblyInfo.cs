9a7d812d08784718352174b5cc8971e972bc2703
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2024-11-04 17:03:10 +0100

    Setup Ingress Nginx controller as API Gateway

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8cbb27a8cc7a4cc6213bf94ebd316b5a8135fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e021cbf97f5a19ec0e785154c014bb62cd8a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
