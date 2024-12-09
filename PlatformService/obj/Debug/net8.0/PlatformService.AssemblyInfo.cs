b49b635a9020b8612191fd4a01b0b4a212cb6afa
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2024-12-09 15:28:25 +0100

    Added Grpc package

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48332cafe1db18e306a4c3973c097c31ead3ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca9a917e0e933b28436f05cfcfcc9db5b0a5ad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
