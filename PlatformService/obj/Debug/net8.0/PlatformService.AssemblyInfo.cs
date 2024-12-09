aeb60541bb8fc987622d279e8bcdf9ab07eabb29
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2024-12-09 15:13:38 +0100

    Updated PlatformService

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22f7bcde91536621adcb67f708b170a1dec7ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48332cafe1db18e306a4c3973c097c31ead3ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
